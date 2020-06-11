d9593cedb226418dc1b152de98058b910b02a520
jamescook98 <63876549+jamescook98@users.noreply.github.com>
2020-06-11 15:06:27 -0500

    added clear button, comments, save icon, etc

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
+//initialize moment.js, display date/time on screen
 var now = moment().format('MMMM Do YYYY, h:mm a');
-
 $("#currentDateTime").text(now);
 
 var hoursArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 for (i = 0; i < hoursArray.length; i++) {
+    //dynamically generate rows/columns for each hour in array
     $('#calendar').append(
         $('<div class="col-2" id="hour">').text(hoursArray[i]),
         $('<textarea class="col-8" id="task">').text(localStorage.getItem("savedTask" + (i + 1))),
-        $('<button class="col-2" id="save">').text("Save"),
+        $('<button class="col-2" id="save">').html("<img src='saveicon.png' width='20px' height='20px'>"),
     );
+    //assign generated divs unique IDs
     $('#task').attr("id", "task" + (i + 1));
     $('#save').attr("id", "task" + (i + 1));
 }
 
+//color code divs based on current time
 timeColor();
 
+function timeColor() {
+    for (var i = 0; i < hoursArray.length; i++) {
+        //if you change "9" in the line below you can test the function at other times of the day (+1 moves it up 1 hour)
+        var time = moment().hour(i + 9);
+        if (moment().isAfter(time)) {
+            $("#task" + (i + 1)).css("background-color", "lightgrey");
+        } else if (moment().isBefore(time)) {
+            $("#task" + (i + 1)).css("background-color", "#99e699");
+        } else {
+            $("#task" + (i + 1)).css("background-color", "#46d246");;
+        }
+    };
+};
+
+//save edited tasks to local storage
 $("button").click(function () {
     event.preventDefault();
     var selectedID = $(this).attr("id");
@@ -24,23 +42,8 @@ $("button").click(function () {
     localStorage.setItem(("savedTask" + selectedID), storedText);
 });
 
-function timeColor() {
-    for (var i = 0; i < hoursArray.length; i++) {
-        var time = moment().hour(i + 9);
-        if (moment().isAfter(time)) {
-            console.log("after");
-            $("#task" + (i + 1)).css("background-color", "lightgreen");
-        } else if (moment().isBefore(time)) {
-            console.log("before");
-            $("#task" + (i + 1)).css("background-color", "lightgrey");
-        } else {
-            console.log("now");
-            $("#task" + (i + 1)).css("background-color", "salmon");;
-        }
-    };
-};
-
+//clear textareas and local storage
 $("#clear").click(function () {
-    event.preventDefault();
-    localStorage.clear;
+    localStorage.clear();
+    location.reload();
 });
